b3ef3e2bf0e9a73bcf36aefadcee181dfe32f3b4
Marcos Rivera <mrivera22@mail.greenriver.edu>
2020-01-22 23:38:38 -0500

    Added inpyt field and button, created function for heehaw, user inputs number and heehaw function will be executed when button is pressed.

--- heehaw.js
+++ heehaw.js
@@ -1,14 +1,26 @@
-for(var i = 1; i < 101; i++) {
-    if(i % 3 == 0 && i % 5 == 0) {
-        console.log("Hee Haw!");
-    }
-    else if(i % 3 == 0) {
-        console.log("Hee!");
-    }
-    else if(i % 5 == 0) {
-        console.log("Haw!");
+const btnPress = document.getElementById("btn");
+const pTag = document.getElementById("placeAnswer");
+
+btnPress.onclick = heeHaw;
+
+function heeHaw() {
+    var input = document.getElementById("input").value;
+
+    if(isNaN(input)) {
+        window.alert("Invalid input! Please provide a number!");
     }
     else {
-        console.log(i);
+        pTag.innerHTML = "";
+        for(var i = 1; i <= input; i++) {
+            if (i % 3 === 0 && i % 5 === 0) {
+                pTag.innerHTML += "Hee Haw!<br>";
+            } else if (i % 3 === 0) {
+                pTag.innerHTML += "Hee!<br>";
+            } else if (i % 5 === 0) {
+                pTag.innerHTML +="Haw!<br>";
+            } else {
+                pTag.innerHTML += i + "<br>";
+            }
+        }
     }
 }
